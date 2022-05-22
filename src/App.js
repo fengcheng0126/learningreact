8276235e532b9674dbead315c6b1b4f2d5cc0f6a
fengcheng0126 <fengcheng0126@gmail.com>
2022-05-21 21:31:48 -0500

    added project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Let's get started!!!</h2>
+      <ExpenseItem></ExpenseItem>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
