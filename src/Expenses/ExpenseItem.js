6e891304d8f2690a2c84c6ff95c3a278310ad4d0
fengcheng0126 <fengcheng0126@gmail.com>
2022-06-01 15:53:52 -0500

    change title button

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -2,14 +2,21 @@ import Card from '../UI/Card.js';
 import ExpenseDate from './ExpenseDate.js';
 import './ExpenseItem.css';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+
+  let title = props.title
+
+  const clickHandler = () => {
+    title = 'Updated!';
+  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
