698a854963f4cdda024887adc9f865894949320b
fengcheng0126 <fengcheng0126@gmail.com>
2022-06-03 17:01:30 -0500

    forms added

--- src/NewExpense/ExpenseForm.js
+++ src/NewExpense/ExpenseForm.js
@@ -1,9 +1,24 @@
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    return <form>
-        <div className='new-expense__controls'></div>
-    </form>;
+    return (
+    <form>
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <imput type='text' />
+            </div>
+            <div className='new-expense__control'>
+                <label>Amount</label>
+                <input type='number' min='0.01' step='0.01' />
+            </div>
+            <div className='new-expense__control'>
+                <label>Date</label>
+                <input type='date' min='2022-06-01' max='2022-12-31' />
+            </div>
+        </div>
+    </form>
+    )  
 };
 
 export default ExpenseForm;
